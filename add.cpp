26dda4a2f87dd20b970a8fa707330338da005c04
seung.hwan <46323118+elbicuderri@users.noreply.github.com>
2021-02-25 21:33:12 +0900

    Update add.cpp

--- add.cpp
+++ add.cpp
@@ -2,45 +2,37 @@
 #include "device_launch_parameters.h"
 
 #include <iostream>
-#include <memory>
+#include <cstdlib>
 
 void addWithCuda(int *c, int *a, int *b, int size);
 
 int main()
 {
+
 	int size = 5;
 
-	int* a = new int[size] {1, 2, 3, 4, 5};
-	int* b = new int[size] {10, 20, 30, 40, 50};
-	int* c = new int[size];
+	////int* a = new int[size] {1, 2, 3, 4, 5};
+	////int* b = new int[size] {10, 20, 30, 40, 50};
+	////int* c = new int[size];
 
-	//int* a = (int*)malloc(size * sizeof(int));
-	//int* b = (int*)malloc(size * sizeof(int));
-	//int* c = (int*)malloc(size * sizeof(int));
-	
-	/*
 	int* a = (int*)std::malloc(size * sizeof(int));
 	int* b = (int*)std::malloc(size * sizeof(int));
 	int* c = (int*)std::malloc(size * sizeof(int));
 
-	if (a == nullptr) return -1;
-	if (b == nullptr) return -1;
-	if (c == nullptr) return -1;
-
-	int aa[5] = { 1, 2, 3, 4, 5 };
-	int bb[5] = { 10, 20, 30, 40, 50 };
-	a = aa;
-	b = bb;
-	*/
-	
-	for (int i = 0; i < 0; i++) {
-		std::cout << a[i] << std::endl;
-		//printf("%d\n", a[i]);
+	if (a)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			a[i] = i + 1;
+		}
 	}
 
-	for (int i = 0; i < 0; i++) {
-		std::cout << b[i] << std::endl;
-		//printf("%d\n", b[i]);
+	if (b)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			b[i] = 10 * (i + 1);
+		}
 	}
 
 	int* d_a;
@@ -56,35 +48,23 @@ int main()
 
 	// Add vectors in parallel.
 	addWithCuda(d_c, d_a, d_b, size);
-	//cudaThreadSynchronize();
 
 	cudaMemcpy(c, d_c, size * sizeof(int), cudaMemcpyDeviceToHost);
 
 	printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
 		c[0], c[1], c[2], c[3], c[4]);
 
-	//printf("{1,2,3,4,5} + {10,20,30,40,50} = {%f,%f,%f,%f,%f}\n",
-	//	c[0], c[1], c[2], c[3], c[4]);
-
-	for (int i = 0; i < size; i++) {
-		printf("%d\n", c[i]);
-	}
-
-	//for (int i = 0; i < 5; i++) {
-	//	std::cout << c[i] << std::endl;
-	//}
-
 	cudaFree(d_a);
 	cudaFree(d_b);
 	cudaFree(d_c);
 
-	//free(a);
-	//free(b);
-	//free(c);
+	std::free(a);
+	std::free(b);
+	std::free(c);
 
-	delete[] a;
-	delete[] b;
-	delete[] c;
+	////delete[] a;
+	////delete[] b;
+	////delete[] c;
 
 	return 0;
 }
